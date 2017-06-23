3855d58c4c1fd7bd4787bee5ab0bfff48261dcdf
郑键 <512472346@qq.com>
2017-06-23 20:51:11 +0800

    添加Mine模块 导入react-native-tab-navigator组件

--- ShopStudy_RN/src/Root.js
+++ ShopStudy_RN/src/Root.js
@@ -7,15 +7,39 @@
 import React, { Component } from 'react';
 import {
     StyleSheet,
-    Text,
-    View
+    View,
 } from 'react-native';
+
+//  导入系统及第三方组件
+import TabNavigator from 'react-native-tab-navigator'
+
+//  自定义组件
+import Home from './Compoment/Home/Home'
+import Shop from './Compoment/Shop/Shop'
+import Mine from './Compoment/Mine/Mine'
+import More from './Compoment/More/More'
 
 export default class ShopStudy_RN extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>项目开始咯</Text>
+
+                {/* 根据不同平台，做是配处理 */}
+                {/*<Text>我是{Platform.OS === 'ios' ? 'iOS' : 'android'}</Text>*/}
+
+                <TabNavigator>
+
+                    {/* 首页 */}
+
+                    {/* 商品 */}
+
+                    {/* 我的 */}
+
+                    {/* 更多 */}
+
+
+                </TabNavigator>
+
             </View>
         );
     }
